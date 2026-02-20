17eeb17763f0b7e36f539d4c69a4cc9356ed46e4
윤지석 <yunjiseok@yunjiseog-ui-MacBookPro.local>
2026-02-20 17:05:01 +0900

    수정

--- 코드트리/2025_ha_mo_2.py
+++ 코드트리/2025_ha_mo_2.py
@@ -11,53 +11,47 @@ def change(id, p):
     ships[id]['p'] = p
     heapq.heappush(hq,(-p, id))
 
+def process_cooldown():
+    while cooldown_hq and cooldown_hq[0][0] <= current_time:
+        ready_time, sid = heapq.heappop(cooldown_hq)
+        heapq.heappush(hq, (-ships[sid]['p'], sid))
 
 def attack():
-    sum = 0
+    total_p = 0
     cnt = 0
     ship_id_list = []
 
-    temp = []
     while hq and cnt<5:
         p, ship_id = heapq.heappop(hq)
         p = -p
 
-        if cnt==5:
-            return sum, cnt, ship_id_list
-        if ships[ship_id]['p'] == p:
-            if ships[ship_id]['ready_time']  > 0:
-                temp.append((-p, ship_id))
-                continue
-            else:
-                sum += p
-                ships[ship_id]['ready_time'] = ships[ship_id]['r']
-                ship_id_list.append(ship_id)
-                temp.append((-p, ship_id))
-
-                cnt+=1
-        else:
+        if ships[ship_id]['p'] != p:
             continue
-    for item in temp:
-        heapq.heappush(hq, item)
 
-    return sum, cnt, ship_id_list
-
-def time_update():
-    for i in ship_id_list:
-
-        if ships[i]['ready_time'] > 0:
-            ships[i]['ready_time'] -= 1
-        else:
+        if ships[ship_id]['ready_time'] > current_time:
             continue
+        else:
+            total_p += p
+            ships[ship_id]['ready_time'] = current_time +  ships[ship_id]['r']
+            heapq.heappush(cooldown_hq, (ships[ship_id]['ready_time'] , ship_id))
+
+            ship_id_list.append(ship_id)
+
+            cnt += 1
+
+    return total_p, cnt, ship_id_list
+
+
 
 N = int(input())
 
-time = 0
+current_time = 0
 
 command_line = list(map(int, input().split()))
 command = command_line[0]
 
 hq = []
+cooldown_hq = []
 ship_id_list = []
 ships = {}
 
@@ -72,28 +66,30 @@ for i in range(command_line[1]):
     ship_id_list.append(ship_id)
     cnt+=3
 
-
+current_time += 1
 
 for _ in range(N-1):
     command_line = list(map(int, input().split()))
     command = command_line[0]
+
+    process_cooldown()
 
     if command == 200:
         id = command_line[1]
         p = command_line[2]
         r = command_line[3]
         request(id,p,r)
-        time_update()
+
 
     elif command == 300:
         id = command_line[1]
         p = command_line[2]
 
         change(id,p)
-        time_update()
+
     elif command == 400:
 
 
         x,y,z = attack()
         print(x,y,*z)
-        time_update()
+    current_time += 1
