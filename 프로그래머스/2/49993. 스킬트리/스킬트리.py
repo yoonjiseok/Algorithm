af7beb61475cdb501a9afd72fcc4e1dfcfd31fea
yoonjiseok <33974157+yoonjiseok@users.noreply.github.com>
2026-04-04 20:39:37 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,24 +1,14 @@
 def solution(skill, skill_trees):
     
-    word = [i for i in skill]
-    
     cnt = 0
-    for i in range(len(skill_trees)):
+    for skills in skill_trees:
         st = 0
-        is_found = False
-        for j in range(len(skill_trees[i])):
-            if is_found:
-                break
-            else:
-                for l in range(len(skill)):
-                    if skill[l] == skill_trees[i][j]:
-                        if l != st:
-                            is_found = True
-                            break
-                        else:
-                            st += 1
-        if not is_found:
-            cnt += 1
-    
-    
+        for j in skills:
+            if j in skill:
+                if j == skill[st]:
+                    st += 1
+                else:
+                    break
+        else:
+            cnt+=1
     return cnt
