129c7726bb5b25983524990adffca1e95f77978f
yoonjiseok <33974157+yoonjiseok@users.noreply.github.com>
2026-04-06 01:37:05 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,28 @@
-def bfs(si,sj,ei,ej):
-    q = []
-    q.append((si,sj))
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-    v[si][sj] = 1
-
+def bfs(st,si, et,ei):
+    q = deque()
+    q.append((st,si))
+    v[st][si] = 1
     while q:
-        x,y = q.pop(0)
+        x,y = q.popleft()
 
-        if ((x,y) == (ei, ej)):
+        if x == et and y == ei:
             return v[x][y]
 
-        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni, nj = x+di, y+dj
-            if (0<= ni < N and 0<= nj < M and adj[ni][nj] == 1 and v[ni][nj] == 0):
-                q.append((ni,nj))
-                v[ni][nj] = v[x][y] + 1
+        for dx,dy in (-1,0),(0,1),(0,-1),(1,0):
+            nx,ny = x+dx, y+dy
+
+            if (0<=nx<=et and 0<=ny<=ei and arr[nx][ny] == 1 and v[nx][ny] == 0):
+                q.append((nx,ny))
+                v[nx][ny] = v[x][y] + 1
 
 
 
-N,M = map(int, input().split())
-
-adj = [list(map(int, input())) for _ in range(N)]
-
-
-v = [[0] * M for _ in range(N)]
+N,M = map(int,input().split(' '))
+v = [[0]*M for _ in range(N)]
+arr = [list(map(int, input().strip())) for _ in range(N)]
 
 print(bfs(0,0,N-1,M-1))
