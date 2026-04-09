c1bb3183a458efb08e4b95035577d928be60ab0f
yoonjiseok <33974157+yoonjiseok@users.noreply.github.com>
2026-04-09 23:05:56 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,20 @@
-from collections import deque
 def solution(n, computers):
     answer = 0
     v = [0] * n
     
-    def bfs(l):
-        q = deque()
-        q.append(l)
-        
-        
-        while q:
-            x = q.popleft()
-            for i in range(n):
-                if v[i] == 0 and computers[x][i] == 1:
-                    v[i] = 1
-                    q.append(i)
+    def dfs(x):
+
+        v[x] = 1 
+        for i in range(n):
+            if computers[x][i] == 1 and v[i] == 0:
+
+                dfs(i)
+                
+                
     
     for i in range(n):
         if v[i] == 0:
-            v[i] = 1
-            bfs(i)
+            dfs(i) 
             answer += 1
             
     return answer
