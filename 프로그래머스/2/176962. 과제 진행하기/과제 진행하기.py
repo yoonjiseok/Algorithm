ef66b7f82fc8e89e2d225f9301d17de6d8ecdb2b
yoonjiseok <33974157+yoonjiseok@users.noreply.github.com>
2026-09-23 20:53:44 +0900

    [level 2] Title: 과제 진행하기, Time: 1.20 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,52 +1,83 @@
 def solution(plans):
+    waiting = []
+    q = {}
     
-    subject = {}
-    stack = []
-    answer = []
-    cnt = 0
-
+    for i in range(len(plans)):
+        temp = plans[i][1].split(":")
+        t1 = temp[0]
+        t2 = temp[1]
+        t1 = int(t1)
+        t2 = int(t2)
+        
+        t = t1*60 + t2
+        
+        tl = plans[i][2]
+        tl = int(tl)
+        
+        plans[i][1] = t
+        plans[i][2] = tl
+        
+        q[plans[i][0]] = {'st' : t , 're' : tl}
+    
+    
     plans.sort(key=lambda x : x[1])
-    for i in plans:
-        h = int(i[1][0:2]) * 60
-        m = int(i[1][3:])
-        
-        subject[cnt] = {'subject':i[0] , 'start' : h+m, 't':int(i[2])}
-        cnt += 1
-        
-  
-
-    for i in range(len(plans)-1):
-        id, start, play_time = i,subject[i]['start'], subject[i]['t']
-        next_start = subject [i+1]['start']
-        available_time = next_start - start
-        
-        if play_time <= available_time:
-            answer.append(subject[id]['subject'])
-            available_time = available_time - play_time
-            
-            
-            while available_time > 0 and stack:
-                temp_id, temp_time = stack.pop()
-                
-                if available_time >= temp_time:
-                    available_time -= temp_time
-                    answer.append(subject[temp_id]['subject'])
-                else:
-                    temp_time -= available_time
-                    stack.append((temp_id, temp_time))
-                    break
-
-        else:
-            rest_time = play_time - available_time
-            stack.append((id,rest_time))        
     
-    answer.append(plans[-1][0])
-    while stack:
-        id, re_time = stack.pop()
-        answer.append(subject[id]['subject'])
+    time = plans[0][1]
+    
+    answer = []
+    
+    len_plans = len(plans)
+    
+    X,Y,Z = plans.pop(0)
+    
+    while len(answer) != len_plans:
+        #plans 가 있다면
+        if plans:
+            t_X,t_Y,t_Z = plans.pop(0)
+            
+            #한번에 처리가 가능한지
+            if Y+Z <= t_Y:
+                answer.append(X)
+                #대기하는 작업이 있는지
+                if waiting:
+                    
+                    now = Y+Z
+                    
+                    while waiting:
+                        w_X, w_Y, w_Z = waiting.pop()  
+                        
+                        if now+w_Z <= t_Y:
+                            answer.append(w_X)
+                            now = now+w_Z
+                        else:
+                            gap = t_Y - now
+                            
+                            waiting.append((w_X, t_Y, w_Z - gap))
+                            break
+
+                #대기하는 작없이 없다
+                
+                
+                
+                X,Y,Z = t_X,t_Y,t_Z
+            
+            #한번에 처리가 불가능함
+            else:
+                gap = t_Y - Y
+                waiting.append((X,t_Y, Z-gap))
+                X,Y,Z = t_X,t_Y,t_Z
+        
+        #plans가 없다면
+        else:
+            answer.append(X)
+            while waiting:
+                w_X, w_Y, w_Z = waiting.pop()
+
+                answer.append(w_X)
+                
             
             
-            
-            
-            
+            break
+        
+    
     return answer
